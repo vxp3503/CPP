4d309840b9bfabf3af2732ca931e3166e6b72c02
Vikrant Verma <vikrantverma725@gmail.com>
2020-11-25 23:14:11 +0530

    Graph Starting

---
+++ Graph/Graph.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Graph{
+    public:
+    int v;
+    list <int> *adj;
+    Graph(int V){
+        v=V;
+        adj =new list <int>[V];
+    }
+    void addedge(int V,int W){
+        adj[V].push_back(W);
+    }
+    void print(){
+        list <int>::iterator i;
+        i=adj[0].begin() ;
+        cout<<"0";
+        while(i!=adj[0].end()){
+            cout<<"->"<<*i;
+            i++;
+        }
+
+    }
+};
+
+
+int main(){
+    Graph g(4);
+    g.addedge(0,5);
+    g.addedge(0,6);
+    g.print();
+}
