6f97322b41b63362e76dd9b1e74b026f40b69275
vxp3503 <vikrantverma725@gmail.com>
2021-02-08 23:54:08 +0530

    STL

---
+++ STL/Array.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    array<int, 5> viki={1,2,3,4,5};
+    cout<<viki.size()<<endl;;
+    cout<<viki.front()<<endl;
+    cout<<*viki.data();
+
+}
