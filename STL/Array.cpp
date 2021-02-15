aacdb05c4b2205e9b4bb01e39756a2ed7f5b5025
vxp3503 <vikrantverma725@gmail.com>
2021-02-15 21:29:09 +0530

    Updated

--- STL/Array.cpp
+++ STL/Array.cpp
@@ -3,8 +3,7 @@ using namespace std;
 int main()
 {
     array<int, 5> viki={1,2,3,4,5};
-    cout<<viki.size()<<endl;;
+    cout<<viki.size()<<endl;
     cout<<viki.front()<<endl;
     cout<<*viki.data();
-
 }
