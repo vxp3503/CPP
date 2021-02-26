db03b04a146fee1b74c80e1c8fa6a806f0217e29
vxp3503 <vikrantverma725@gmail.com>
2021-02-27 00:32:05 +0530

    Update

---
+++ Array/Duplicates.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the no. of array";
+    cin>>n;
+    vector<int>a;
+    int item;
+    for(int i=0;i<n;i++)
+    {
+        cin>>item;
+        a.push_back(item);
+    }
+    sort(a.begin(),a.end());
+    for(int i=0;i<n;i++)
+    {
+        if(i>0&&a[i]==a[i-1])
+        {
+            continue;
+        }
+        if(a[i]==a[i+1])
+        {
+            cout<<a[i];
+        }
+    }
+}
