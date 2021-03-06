0231080139ef881d89818afe8b409a76efb3ba10
vxp3503 <vikrantverma725@gmail.com>
2021-03-06 18:36:39 +0530

    Update

---
+++ Stack/Reverse_a_string_using_stack.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int reverse(string &s)
+{
+    stack<char>v;
+    for (int i = 0; i < s.size(); i++)
+    {
+        v.push(s[i]);
+    }
+    for (int i = 0; i < s.size(); i++)
+    {
+        s[i]=v.top();
+        v.pop();
+    }
+    return 0;
+}
+int main()
+{
+    string s;
+    cout<<"Enter String";
+    cin>>s;
+    cout<<"After revers string becomes";
+    reverse(s);
+    cout<<endl<<s;
+}
