d29945e0edd4e41f63191bcaedcaa62c386ad994
vxp3503 <vikrantverma725@gmail.com>
2021-03-17 23:52:25 +0530

    Update

--- Array/Largest_sum_contiguous_sub_array.cpp
+++ Array/Largest_sum_contiguous_sub_array.cpp
@@ -1,26 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int lrgestsuminsubarray(int a[],int n)
+int lrgestsuminsubarray(int a[], int n)
 {
-    int curr=a[0];
-    int maxi=a[0];
-    for(int i=1;i<n;i++)
+    int curr = a[0];
+    int maxi = a[0];
+    for (int i = 1; i < n; i++)
     {
-        curr=max(a[i],curr+a[i]);
-        maxi=max(curr,maxi);
+        curr = max(a[i], curr + a[i]);
+        maxi = max(curr, maxi);
     }
     return maxi;
 }
 int main()
 {
     int n;
-    cout<<"Enter no. of elements";
-    cin>>n;
+    cout << "Enter no. of elements";
+    cin >> n;
     int a[n];
-    cout<<"Enter Elements";
+    cout << "Enter Elements";
     for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
-    cout<<lrgestsuminsubarray(a,n);
+    cout << lrgestsuminsubarray(a, n);
 }
