d198893b00314c20247e31645acb8993b09d139f
vxp3503 <vikrantverma725@gmail.com>
2021-03-24 22:15:45 +0530

    Update

---
+++ Array/Minimize_the_hight_II.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int getMinDiff(int arr[],int n,int k)
+{
+        sort(arr,arr+n);
+        int  maxi;int mini;
+        int ans=arr[n-1]-arr[0];
+        for(int i=1;i<=n-1;i++)          //-ve height is not considered
+        {
+            if(arr[i]>=k&&arr[i-1]>=k)
+            {
+            maxi=max(arr[i-1]+k,arr[n-1]-k);
+             mini=min(arr[0]+k,arr[i]-k);
+            ans=min(ans,maxi-mini);
+            }
+            else
+            continue;
+        }
+        return ans;
+}
+int main()
+{
+	int arr[] = {6, 1, 9, 1, 1, 7, 9, 5, 2, 10};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	int k = 4;
+	cout << "\nMaximum difference is "
+		<< getMinDiff(arr, n, k);
+	return 0;
+}
